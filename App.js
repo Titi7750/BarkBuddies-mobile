2f973ae47bf78ac13258e39005c09451a741b4e0
Titi <95020920+Tristitititi@users.noreply.github.com>
2023-04-25 16:14:43 +0200

    Install project in React Native & Tinder card UI

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, Image, ImageBackground, View, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.pageContainer}>
+      <View style={styles.card}>
+        <ImageBackground source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png' }} style={styles.ImageBackground}>
+          <View style={styles.cardInner}>
+            <Text style={styles.name}>Elon Musk</Text>
+            <Text style={styles.bio}>A dude with a rocket is looking for a gal with fuel</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  pageContainer: {
+    flex: 1, justifyContent: 'center', alignItems: 'center'
+  },
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+
+    elevation: 11,
+  },
+  ImageBackground: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+  cardInner: {
+    padding: 10,
+  },
+  name: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  bio: {
+    color: 'white',
+    fontSize: 18,
+    lineHeight: 25,
   },
 });
+
+export default App;
